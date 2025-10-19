a3c23b62dbac004d9cf505d39ba6d0b791bc667d
bob4sad <bob4.work13@gmail.com>
2025-10-19 07:00:03 +0300

    third session

---
+++ 1/1.11/float.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a float64
+	fmt.Scan(&a)
+	switch {
+	case a <= 0.0:
+		fmt.Printf("число %2.2f не подходит", a)
+	case a > 10000.0:
+		fmt.Printf("%e", a)
+	default:
+		fmt.Printf("%.4f", a*a)
+	}
+}
